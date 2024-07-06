46dd8d3e2babcd44326d16d5da34437c3c612768
sujalrawat884 <129302673+sujalrawat884@users.noreply.github.com>
2024-07-06 19:18:06 +0530

    Update Homepage.py

---
+++ Homepage.py
@@ -0,0 +1,33 @@
+import streamlit as st 
+from streamlit_option_menu import option_menu
+
+
+st.markdown("""
+<style>
+[data-testid="stAppViewContainer"] {
+background-image: url("https://images.unsplash.com/photo-1628348070889-cb656235b4eb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+background-size: cover;
+}
+[data-testid="stHeader"] {
+background-color: rgba(0, 0, 0, 0);  
+}
+[data-testid="stToolbar"] {
+right: 2rem;
+}
+.st-emotion-cache-czk5ss.e16jpq800{
+    visibility: hidden;
+}
+.st-emotion-cache-mnu3yk.ef3psqc5{
+    visibility: hidden;
+}
+.st-emotion-cache-15ecox0.ezrtsby0{
+    visibility: hidden;
+}
+</style>            
+""", unsafe_allow_html=True)
+
+
+st.markdown("<h1 style='text-align: center; color: black;'>Welcome To The Homepage</h1>",unsafe_allow_html=True)
+
+    
+    
